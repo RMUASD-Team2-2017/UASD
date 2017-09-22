c4854e7fb660b693420cf1108a580af507eca4fa
Tobias Lundby <tobiaslundby@gmail.com>
2017-09-22 20:51:47 +0200

    Added API for setting the comment

---
+++ WEB/set_drone_comment.php
@@ -0,0 +1,29 @@
+<?php
+    // Require DB config
+    require('config.php');
+    // Connect to database
+    $con = mysqli_connect($mysql_host, $mysql_user, $mysql_pw, $mysql_db);
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $drone_id = $_POST['drone_id'];
+    $comment = $_POST['comment'];
+
+    $sql = "UPDATE `AED_drone_list` SET `comment` = '$comment' WHERE  `id` = '$drone_id'";
+    $result = mysqli_query($con, $sql);          //query
+
+    if(mysqli_affected_rows($con) == 1) {
+        //die('Could not update data: ' . mysql_error());
+        $array_out = array(
+            "status" => 1,
+        );
+    } else {
+        $array_out = array(
+            "status" => 0,
+        );
+    }
+    echo json_encode($array_out);
+
+   mysqli_close($con);
+?>
