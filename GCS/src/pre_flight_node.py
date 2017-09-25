d2f1cf4ebcf6458e16ae3b8a6631329d57929386
Bjarki Sigurdsson <bjarkips@gmail.com>
2017-09-25 14:24:31 +0200

    Broke pre-flight node

--- GCS/src/pre_flight_node.py
+++ GCS/src/pre_flight_node.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python
 import rospy
+from gcs.srv import *
 from std_msgs.msg import String
 from weather import Weather
 
+def handle_pre_flight_srv():
+    return preFlightResponse()
+
 def pre_flight():
-    pub = rospy.Publisher('weather_data', String, queue_size=10)
-    rospy.init_node('pre_flight', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rospy.init_node('pre_flight')
     weather = Weather();
-    weather.setLocation(55.471089000000006, 10.330159499999999, 1)
-
-    while not rospy.is_shutdown():
-        print(weather.getTemperature() + "C")
-        print(weather.getWindDirection())
-        print(weather.getWindSpeed() + "m/s")
-        print("Wind Category: " + weather.getWindCategory())
-        print("Humidity: " + weather.getHumidity() + "%")
-
-        example_str = weather.getWindSpeed() + "m/s"
-        pub.publish(example_str)
-        rate.sleep()
-
+    # TODO get actual location
+    weather.setLocation(55.471089000000006, 10.330159499999999, 1) 
+    pfs = rospy.Service('pre_flight_srv', preFlight, handle_pre_flight_srv)
+    rospy.spin()
+	    
 if __name__ == '__main__':
     try:
         pre_flight()
