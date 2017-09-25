cf51380ccdd28be1cc61ad8108c3303930e91dd0
Bjarki Sigurdsson <bjarkips@gmail.com>
2017-09-25 14:59:42 +0200

    Implemented rospy preflight service

--- GCS/src/pre_flight_node.py
+++ GCS/src/pre_flight_node.py
@@ -4,15 +4,15 @@ from gcs.srv import *
 from std_msgs.msg import String
 from weather import Weather
 
-def handle_pre_flight_srv():
-    return preFlightResponse()
-
-def pre_flight():
-    rospy.init_node('pre_flight')
+def handle_pre_flight_srv(req):
     weather = Weather();
     # TODO get actual location
     weather.setLocation(55.471089000000006, 10.330159499999999, 1) 
-    pfs = rospy.Service('pre_flight_srv', preFlight, handle_pre_flight_srv)
+    return preFlightResponse( weather.getWindSpeed() < 15 )
+
+def pre_flight():
+    rospy.init_node('pre_flight')
+    pfs = rospy.Service('pre_flight_node/preflight', preFlight, handle_pre_flight_srv)
     rospy.spin()
 	    
 if __name__ == '__main__':
