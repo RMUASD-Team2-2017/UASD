aced89645daea791da92109f4b689a46a3ad120f
Mathias Hoejgaard Egeberg <mhoejgaard@hotmail.com>
2017-12-05 12:52:53 +0100

    Added publishing of r reques_id in web_interface

--- GCS/src/web_interface_node.py
+++ GCS/src/web_interface_node.py
@@ -5,6 +5,7 @@ import time
 from gcs.msg import waypoint, setPreflightData, deploy_request
 from std_msgs.msg import Bool, String
 from sensor_msgs.msg import NavSatFix
+from std_msgs.msg import String
 
 drone_number = 1
 interface = web_interface.web_interface()
@@ -37,6 +38,8 @@ def main():
     ### Setup ROS ###
     rospy.init_node('web_interface')
     deploy_request_publisher = rospy.Publisher('/web_interface/deploy_request',deploy_request, queue_size=1)
+    request_id_publisher = rospy.Publisher('/web_interface/request_id', String, queue_size=1)
+
     mission_done_Subscriber = rospy.Subscriber('web_interface/listen/mission_done',Bool, missionDoneCallback)
     set_preflight_data_Subscriber = rospy.Subscriber('/web_interface/listen/set_preflight_data',setPreflightData,setPreflightDataCallback)
     set_uav_state_subscriber = rospy.Subscriber('/web_interface/listen/set_uav_state',String,setUavStateCallback)
@@ -46,7 +49,7 @@ def main():
     rate = rospy.Rate(1)
     rospy.loginfo("[web_interface] Node started.")
     while not rospy.is_shutdown():
-        (update_count,lat,lng,alt) = interface.getDeployRequest()
+        (update_count,lat,lng,alt, request_id) = interface.getDeployRequest()
         print 'Checking',update_count,lat,lng,alt
         if update_count == 0:
             msg = deploy_request()
@@ -54,6 +57,9 @@ def main():
             msg.point.lon = lng
             msg.point.alt = alt
             deploy_request_publisher.publish(msg)
+            request_id_msg = String()
+            request_id_msg.data = request_id
+            request_id_publisher.publish(request_id_msg)
             print 'Published deploy request'
             rospy.loginfo(msg)
         rate.sleep()
