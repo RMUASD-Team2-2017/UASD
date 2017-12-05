5c293360a86f7ab72b7eab3221aa170fede7765c
Tobias Lundby <tobiaslundby@gmail.com>
2017-12-05 20:13:16 +0100

    Added functionality for disapproving a request, this feature along with the complete request can de disabled by setting the developer mode in the top of the document

---
+++ WEB/admin/disapprove_request.php
@@ -0,0 +1,26 @@
+<?php
+    // Require DB config
+    require('config.php');
+    // Connect to database
+    $con = mysqli_connect($mysql_host, $mysql_user, $mysql_pw, $mysql_db);
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $request_id = $_GET['id'];
+    if (isset($_GET['id'])) {
+        $sql = "UPDATE `AED_requests` SET `approved` = '0'  WHERE `request_id` = '$request_id'";
+        $result = mysqli_query($con, $sql);          //query
+    }
+
+    mysqli_close($con);
+
+    $page = $_GET['page'];
+    if ($page == 'show_request.php') {
+        header("Location: ".$page."?id=".$request_id);
+    } else {
+        header("Location: ".$page);
+    }
+
+    die();
+?>
